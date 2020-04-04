e1a3f7127f6fe1d763bbc931e8012e16a621d046
shashank0601 <shashankvadalia1@gmail.com>
2020-04-04 17:37:37 +0530

    setting up basic client-server connection

--- client/src/components/AppJoin.js
+++ client/src/components/AppJoin.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const AppJoin = () =>{
+    const [user,setUser] = useState('');
+    const [room,setRoom] = useState('');
     return(
-        <h1>AppJoin</h1>
+        <div className='outerContainer'>
+            <div className='innerContainer'>
+                <h1 className='heading'>Join</h1>
+                <div><input placeholder='' className='input' type='text' onChange={} /></div>
+                <div><input placeholder='' className='input' type='text' onChange={} /></div>
+                <Link>
+                    <button className='button' type='submit'>Sign In</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const router = require('./router')
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(router)
+
+io.on('connection',(socket) =>{
+    console.log('New connection in app');
+    socket.on('disconnect',()=>{
+        console.log('User has left');
+    })
+})
+
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
